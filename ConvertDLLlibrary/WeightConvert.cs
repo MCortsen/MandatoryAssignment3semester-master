7305acc07c48e57e7e966edde30d3a4e02caf9a5
TickleMeeeHomo <33862039+TickleMeeeHomo@users.noreply.github.com>
2018-10-07 16:34:50 +0200

    Initial commit

---
+++ ConvertDLLlibrary/WeightConvert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConvertDLLLibraryG
+{
+    public static class WeightConvert
+    {
+        private const double OzToGram = 28.349;
+        private const double GramToOz = 0.0352;
+
+        public static double OzToGrams(double OZ)
+        {
+            return OZ * OzToGram;
+        }
+
+        public static double GramToOzs(double grams)
+        {
+            return grams * GramToOz;
+        }
+    }
+}
